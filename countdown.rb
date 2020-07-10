129d7b7e1a2eb039641f5695178d8744b9abbeea
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-10 01:18:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,18 +6,15 @@ def countdown(number)
   puts "#{number} SECOND(S)!"
   number-=1
 end
-return "HAPPY NEW YEAR!"
+"HAPPY NEW YEAR!"
 end
 
 countdown(12)
 
 def countdown_with_sleep(number)
   while number>0
-  sleep (1)
  puts "#{number} SECOND(S)!"
+  sleep (1)
   number-=1
 end
-puts "HAPPY NEW YEAR!"
 end
-
-countdown_with_sleep(5)
